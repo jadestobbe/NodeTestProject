b9f1349765d0912fe6f3add48d93837cd618034f
Jade Stobbe <jadestobbe@gmail.com>
2017-11-13 18:14:15 -0600

    added some dummy data to practice api call

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,10 +17,36 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'))
 });
 
+app.get('/users', function(req, res) {
+    //Hardcoding in dummy data
+    res.json([{
+            "id": 1,
+            "firstName": "Bob",
+            "lastName": "Smith",
+            "email": "bob@gmail.com"
+        },
+        {
+            "id": 2,
+            "firstName": "Tammy",
+            "lastName": "Norton",
+            "email": "tammy@gmail.com"
+        },
+        {
+            "id": 3,
+            "firstName": "Tina",
+            "lastName": "Lee",
+            "email": "tina@gmail.com"
+        }
+
+
+    ]);
+});
+
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
     } else {
-        open('https://jade.localtunnel.me');
+        //open('https://jade.localtunnel.me');
+        open('http://localhost:' + port);
     }
 });
